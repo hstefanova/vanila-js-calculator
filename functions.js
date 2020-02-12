2707147217812147d26b8c9c1ebe857dcfe585fd
hristiyana.stefanova <hristiyana.stefanova@mentormate.com>
2020-02-12 18:49:06 +0200

    Only variables

---
+++ functions.js
@@ -0,0 +1,22 @@
+var oneBtn = document.getElementById('calc-one');
+var twoBtn = document.getElementById('calc-two');
+var threeBtn = document.getElementById('calc-three');
+var fourBtn = document.getElementById('calc-four');
+var fiveBtn = document.getElementById('calc-five');
+var sixBtn = document.getElementById('calc-six');
+var sevenBtn = document.getElementById('calc-seven');
+var eightBtn = document.getElementById('calc-eight');
+var nineBtn = document.getElementById('calc-nine');
+var zeroBtn = document.getElementById('calc-zero');
+
+var decimalBtn = document.getElementById('calc-decimal');
+var clearBtn = document.getElementById('calc-clear');
+var backspaceBtn = document.getElementById('calc-backspace');
+
+var plusBtn = document.getElementById('calc-multiply');
+var minusBtn = document.getElementById('calc-minus');
+var multiplyBtn = document.getElementById('calc-multiply');
+var divideBtn = document.getElementById('calc-divide');
+var equalBtn = document.getElementById('calc-equal');
+
+var displayValElement = document.getElementById('calc-display-val');
